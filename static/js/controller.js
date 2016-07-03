4637e501587c6c5413c56542e0ed9489a4bb5d01
Benoit Seguin <seg.benoit@gmail.com>
2016-07-04 01:41:26 +0200

    ui-router functionning

---
+++ static/js/controller.js
@@ -0,0 +1,31 @@
+/**
+ * Created by benoit on 04/07/2016.
+ */
+
+var patatesApp = angular.module('patatesApp', ['ui.bootstrap', 'ui.router']);
+
+patatesApp.config(function ($stateProvider, $urlRouterProvider) {
+
+    //
+    // For any unmatched url, redirect to /search
+    $urlRouterProvider.otherwise("/home");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "static/partials/home.html"
+        })
+        .state('about', {
+            url: "/about",
+            templateUrl: "static/partials/about.html"
+        })
+        .state('contact', {
+            url: "/contact",
+            templateUrl: "static/partials/contact.html"
+        })
+        .state('buy', {
+            url: "/buy",
+            templateUrl: "static/partials/buy.html"
+        })
+});
